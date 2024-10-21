7f6f3c3f3f40b3b6a04af8b6f62b5be13eb653c8
ahmad luqman hakim <ahmad luqman hakim@MSI>
2024-10-21 23:05:01 +0700

    Tugas 6 Latihan PBO part 1

---
+++ src/Kelas/Category.java
@@ -0,0 +1,30 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Kelas;
+
+/**
+ *
+ * @author ahmad luqman hakim
+ */
+public class Category {
+    int category_id;
+    String catrgory_name;
+
+    public int getCategory_id() {
+        return category_id;
+    }
+
+    public void setCategory_id(int category_id) {
+        this.category_id = category_id;
+    }
+
+    public String getCatrgory_name() {
+        return catrgory_name;
+    }
+
+    public void setCatrgory_name(String catrgory_name) {
+        this.catrgory_name = catrgory_name;
+    }
+}
